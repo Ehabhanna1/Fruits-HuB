b18ddc32384689f2476060176f708779f76ce784
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-04-21 16:09:21 +0200

    fake commit

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -11,6 +11,7 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:  SafeArea(child: const SplashViewBody()),
+      
     );
   }
 }
