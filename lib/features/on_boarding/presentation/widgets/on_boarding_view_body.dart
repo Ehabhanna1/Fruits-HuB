b3829368e604d8d3b70b5264e0d9039e05a28072
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-04-22 11:40:09 +0200

    add logic page view p1

--- lib/features/on_boarding/presentation/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/widgets/on_boarding_view_body.dart
@@ -5,8 +5,31 @@ import 'package:fruits_hub_app/core/utils/spacing.dart';
 import 'package:fruits_hub_app/core/widgets/custom_button.dart';
 import 'package:fruits_hub_app/features/on_boarding/presentation/widgets/on_boarding_page_view.dart';
 
-class OnBoardingViewBody extends StatelessWidget {
+class OnBoardingViewBody extends StatefulWidget {
   const OnBoardingViewBody({super.key});
+
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> {
+ late PageController pageController;
+
+ var currentPage = 0;
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      currentPage = pageController.page!.round();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,17 +42,15 @@ class OnBoardingViewBody extends StatelessWidget {
             activeColor: AppColors.primaryColor,
 
             color: AppColors.primaryColor.withOpacity(0.5),
-            
           ),
         ),
         verticalSpacing(20),
 
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: CustomButton(onPressed: (){}, text: 'ابدأ الان'),
+          child: CustomButton(onPressed: () {}, text: 'ابدأ الان'),
         ),
         verticalSpacing(34),
-        
       ],
     );
   }
