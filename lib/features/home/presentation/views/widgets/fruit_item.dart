cc48a27d77f1c802c33ca73fc505c366857f969e
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-05-02 13:15:41 +0300

    create fruit item

---
+++ lib/features/home/presentation/views/widgets/fruit_item.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruits_hub_app/core/utils/app_colors.dart';
+import 'package:fruits_hub_app/core/utils/app_images.dart';
+import 'package:fruits_hub_app/core/utils/app_text.dart';
+import 'package:fruits_hub_app/core/utils/spacing.dart';
+
+class FruitItem extends StatelessWidget {
+  const FruitItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.sizeOf(context).width * 0.4,
+      height: MediaQuery.sizeOf(context).height * 0.27,
+      decoration: BoxDecoration(
+        color: Color(0xFFF3F5F7),
+        borderRadius: BorderRadius.circular(16), 
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            right: 0,
+            top: 0,
+            child: IconButton(onPressed: (){}, icon: const Icon(Icons.favorite_border))),
+
+
+            Positioned.fill(child: Column(
+           
+              children: [
+                verticalSpacing(25),
+                Image.asset(AppImages.imagesWatermelonTest),
+                verticalSpacing(20),
+                ListTile(
+                  title: Text('بطيخ',style: TextStyles.semiBold18),
+                  subtitle: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(text: '20جنية ',style: TextStyles.bold15.copyWith(
+                          color: AppColors.secondaryColor,
+                        )),
+                        TextSpan(text: '/',style: TextStyles.bold15.copyWith(
+                          color: AppColors.lightSecondaryColor,
+                        )),
+                        TextSpan(text: ' ',style: TextStyles.bold15.copyWith(
+                          color: AppColors.lightSecondaryColor,
+                        )),
+                        TextSpan(text: ' الكيلو',style: TextStyles.regular16.copyWith(
+                          color: AppColors.lightSecondaryColor,
+                        )),
+                      ]
+                    )
+                  ),
+                  trailing: const CircleAvatar(
+                    radius: 20,
+                    backgroundColor: AppColors.primaryColor,
+                    child: Icon(Icons.add,color: Colors.white,),
+                  ),
+                
+                ),
+
+                
+              ],
+            ))
+          
+        ], 
+      ),
+    );
+  }
+} 
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -5,6 +5,7 @@ import 'package:fruits_hub_app/core/widgets/search_text_field.dart';
 import 'package:fruits_hub_app/features/home/presentation/views/widgets/best_selling_headers.dart';
 import 'package:fruits_hub_app/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:fruits_hub_app/features/home/presentation/views/widgets/feature_list.dart';
+import 'package:fruits_hub_app/features/home/presentation/views/widgets/fruit_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -26,6 +27,8 @@ class HomeViewBody extends StatelessWidget {
                 FeatureList(),
                 verticalSpacing(15),
                 BestSellingHeaders(),
+                verticalSpacing(10),
+                FruitItem(),
                
               ],
             ),
